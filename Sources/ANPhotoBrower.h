b3d2db93d86bf66f81c1ad789dce07d1268af356
liudong <liudong.edward@gmail.com>
2018-11-28 15:55:09 +0800

    初始化项目

---
+++ Sources/ANPhotoBrower.h
@@ -0,0 +1,19 @@
+//
+//  ANPhotoBrower.h
+//  ANPhotoBrower
+//
+//  Created by 刘栋 on 2018/11/28.
+//  Copyright © 2018 anotheren.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ANPhotoBrower.
+FOUNDATION_EXPORT double ANPhotoBrowerVersionNumber;
+
+//! Project version string for ANPhotoBrower.
+FOUNDATION_EXPORT const unsigned char ANPhotoBrowerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANPhotoBrower/PublicHeader.h>
+
+
